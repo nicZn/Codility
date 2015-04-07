e4b927e9a36876ad89818210d98e3605471b1586
n_cho <n_cho@yumemi.co.jp>
2015-04-07 16:43:21 +0900

    solution for TapeEquilibrium Lession One

---
+++ TapeEquilibrium.c
@@ -0,0 +1,30 @@
+int solution(int A[], int N) {
+    if(N == 0) {
+        return 0;
+    }
+    
+    if (N == 1) {
+        return A[0];
+    }
+    
+    int *sum;
+    sum = (int *)realloc(0, sizeof(int) * N);
+    
+    int singleSum = 0;
+    for (int i = 0; i < N ; i ++) {
+        singleSum += A[i];
+        sum[i] = singleSum;
+    }
+    int result = sum[N - 1];
+    for (int i = 0; i < N - 1; i ++) {
+        int singleMin = abs(sum[N - 1] - 2 * sum[i]);
+        if(result == sum[N -1] && i == 0) {
+            result = singleMin;
+            continue;
+        }
+        if(result > singleMin || result < 0){
+            result = singleMin;
+        }
+    }
+    return result;
+}
